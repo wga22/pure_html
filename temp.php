e998720e74b7f092429cbed48e79d977fa603776
Will <wga22@yahoo.com>
2023-11-01 14:33:59 -0400

    changed weather api

--- temp.php
+++ temp.php
@@ -13,40 +13,72 @@ change over to https://goweatherforecast.com/current?lat=38.8978&lng=-77.2885&se
 1) track downtime of the 2 sites
 
 */
+include 'vars.php';
 
-//CONSTANTS
-$heroURL = 'https://goweather.herokuapp.com/weather/22182-';
-$backupSite= 'https://goweatherforecast.com/current?lat=38.8978&lng=-77.2885&search=&ts=1';
+//CONSTANTS not able to set globally?!
+//$heroURL = 'https://goweather.herokuapp.com/weather/22182';
+//$backupSite= 'https://goweatherforecast.com/current?lat=38.8978&lng=-77.2885&search=&ts=1';
 
-$hTemp = getHeroTemp();
+//$hTemp = getHeroTemp(); //BROKEN for now
+$hTemp = getOpenWeatherMap($LAT, $LNG, $weather_api);
 $temp=-100; //try for something better, but -100 implies something wrong
-//print_r("hero" . $hTemp . "<p>");
+//print_r("hero" . $weather_api . "<p>");
 if( $hTemp > -100)
 {
-    $temp = toFarenheit($hTemp);
+    $temp = KtoFarenheit($hTemp);
 }
 else    //try a second site
 {
     $bkTemp = tryBackupSite();
     if($bkTemp > -100)
     {
-        $temp = toFarenheit($bkTemp);
+        $temp = CtoFarenheit($bkTemp);
     }
 }
 
+//print_r("api:" . $api);
 print_r("temp:" . $temp);
 
-function toFarenheit($celTemp)
+
+function KtoFarenheit($kTemp)
+{
+//(K − 273.15) × 9/5 + 32 = °F
+    return CtoFarenheit($kTemp-273.15);
+}
+
+
+function CtoFarenheit($celTemp)
 {
     return 9/5*$celTemp+32;
 }
 
 
+function getOpenWeatherMap($lat, $lng, $api)
+{
+    $surl = 'https://api.openweathermap.org/data/2.5/weather?lat='.$lat.'&lon='.$lng.'&appid='.$api;
+    //print_r($surl);
+    $res = getSite($surl);
+//TODO: test for valid JSON response elements
+//print_r($res . is_null($res));
+    $return_data = json_decode($res, true);
+//print_r($return_data . "<p>");
+    $temp = floatval($return_data['main']['temp']);
+//print_r($temp . " weather api<p>");
+    if(strlen($temp)==0 || is_null($temp))
+    {
+        //print_r("<p>cannot find hero: " . $temp);
+        $temp=-100;
+    }
+    return $temp;
+}
+
 function getHeroTemp()
 {
     $res = getSite('https://goweather.herokuapp.com/weather/22182');
-    $return_data = json_decode($response, true);
-    $temp = floatval($return_data['temperature']);    
+//print_r($res . is_null($res));
+    $return_data = json_decode($res, true);
+//print_r($return_data . "<p>");
+    $temp = floatval($return_data['temperature']);
     if(strlen($return_data['temperature'])==0 || is_null($temp))
     {
         //print_r("<p>cannot find hero: " . $temp);
@@ -62,7 +94,9 @@ function tryBackupSite()
     $dom = new DomDocument(); 
     $dom->loadHTML(getSite('https://goweatherforecast.com/current?lat=38.8978&lng=-77.2885&search=&ts=1'));   
     $xpath = new DOMXpath($dom);
+//BROKEN print_r($xpath . "zzzzz<p>");
     $currentTemp=parseToArray($xpath,'currentTemperature');
+    $temp = -100;
     if(!is_null($currentTemp) && count($currentTemp)>0)
     {
         $temp=floatval($currentTemp[0]);
@@ -74,13 +108,14 @@ function parseToArray($xpath,$id)
 {
 	$xpathquery="//span[@id='".$id."']";
 	$elements = $xpath->query($xpathquery);
-
+//print_r($elements->length . " elements in the dom list<br>");
 	if (!is_null($elements)) {	
 		$resultarray=array();
 		foreach ($elements as $element) {
 		    $nodes = $element->childNodes;
 		    foreach ($nodes as $node) {
 		      $resultarray[] = $node->nodeValue;
+			print_r($node . "+++<br>");
 		    }
 		}
 		return $resultarray;
@@ -101,7 +136,8 @@ function getSite($getUrl)
     curl_setopt_array($ch, $options);
     $response = curl_exec($ch);
     curl_close($ch);
-    //print_r("<br>URL:" .strlen($getUrl) . '<br>response:' . strlen($response));
+//    print_r("<br>URL:" .strlen($getUrl) . '<br>response length:' . strlen($response));
+//    print_r("<pre>content" .$response . "</pre>");
     return $response;
 }
 
